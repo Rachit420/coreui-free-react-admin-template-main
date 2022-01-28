8dc3359aab5469353a7314c0226b1f9bd5691bc6
rachit <65225697+Rachit420@users.noreply.github.com>
2022-01-28 17:04:05 +0530

    update register page

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -52,85 +52,90 @@ const Register = () => {
 
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
-      <div className="container bg-white p-4">
-        <CForm action="" onSubmit={handlesubmit}>
-          <h1>Register</h1>
-          <p className="text-medium-emphasis">Create your account</p>
-          <CInputGroup className="mb-3">
-            <CInputGroupText>
-              <CIcon icon={cilUser} />
-            </CInputGroupText>
-            <CFormInput
-              placeholder="Full Name"
-              name="fullName"
-              onChange={handleChange}
-              autoComplete="fullname"
-              value={user.fullName}
-              required
-            />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CInputGroupText>@</CInputGroupText>
-            <CFormInput
-              placeholder="Email"
-              type="email"
-              name="username"
-              value={user.username}
-              onChange={handleChange}
-              autoComplete="email"
-              required
-            />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CInputGroupText>
-              <CIcon icon={cilLockLocked} />
-            </CInputGroupText>
-            <CFormInput
-              placeholder="password"
-              type="password"
-              name="password"
-              value={user.password}
-              onChange={handleChange}
-              autoComplete="password"
-              required
-            />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CInputGroupText>
-              <CIcon icon={cilLockLocked} />
-            </CInputGroupText>
-            <CFormInput
-              type="text"
-              name="designation"
-              placeholder="Designation"
-              autoComplete="new-password"
-              required
-              value={user.designation}
-              onChange={handleChange}
-            />
-          </CInputGroup>
-          <CInputGroup className="mb-4">
-            <CInputGroupText>
-              <CIcon icon={cilLockLocked} />
-            </CInputGroupText>
-            <CFormInput
-              type="text"
-              placeholder="Screen Name"
-              autoComplete="screen name"
-              name="screenName"
-              value={user.screenName}
-              onChange={handleChange}
-              required
-            />
-          </CInputGroup>
-          <div className="d-grid">
-            <CButton color="success" type="submit">
-              Create Account
-            </CButton>
-          </div>
-        </CForm>
+      {/* <div className="container bg-white p-4"> */}
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-md-6">
+            <div class="card mb-4 mx-4">
+              <div class="card-body p-4">
+                <form action="" onSubmit={handlesubmit}>
+                  <h1>Register</h1>
+                  <p className="text-medium-emphasis">Create your account</p>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilUser} />
+                    </CInputGroupText>
+                    <CFormInput
+                      placeholder="Full Name"
+                      name="fullName"
+                      onChange={handleChange}
+                      autoComplete="fullname"
+                      value={user.fullName}
+                      required
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>@</CInputGroupText>
+                    <CFormInput
+                      placeholder="Email"
+                      type="email"
+                      name="username"
+                      value={user.username}
+                      onChange={handleChange}
+                      autoComplete="email"
+                      required
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      placeholder="password"
+                      type="password"
+                      name="password"
+                      value={user.password}
+                      onChange={handleChange}
+                      autoComplete="password"
+                      required
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-3">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="text"
+                      name="designation"
+                      placeholder="Designation"
+                      autoComplete="new-password"
+                      required
+                      value={user.designation}
+                      onChange={handleChange}
+                    />
+                  </CInputGroup>
+                  <CInputGroup className="mb-4">
+                    <CInputGroupText>
+                      <CIcon icon={cilLockLocked} />
+                    </CInputGroupText>
+                    <CFormInput
+                      type="text"
+                      placeholder="Screen Name"
+                      autoComplete="screen name"
+                      name="screenName"
+                      value={user.screenName}
+                      onChange={handleChange}
+                      required
+                    />
+                  </CInputGroup>
+                  <div className="d-grid">
+                    <CButton color="success" type="submit">
+                      Create Account
+                    </CButton>
+                  </div>
+                </form>
 
-        {/* <div className="row justify-content-md-center">
+                {/* <div className="row justify-content-md-center">
       <CRow className='justify-content-center'>
           <CCol sm='auto' md='auto'> 
           <CCardGroup>
@@ -212,6 +217,11 @@ const Register = () => {
              </CCol>
             </CRow>
             </div>  */}
+                {/* </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
