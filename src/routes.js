a27e270d4d9e380ec8071f36e32c448a741c748a
rachit <65225697+Rachit420@users.noreply.github.com>
2022-04-05 20:53:12 +0530

    fixed bugs mentioned..
    fixed checkbox issue on create ref page.
    updated edit page.

--- src/routes.js
+++ src/routes.js
@@ -15,12 +15,12 @@ const Meeting = React.lazy(() => import('./views/pages/meeting/Meeting'))
 const ClientList = React.lazy(() => import('./views/pages/clientList/ClientList.jsx'))
 const ProductList = React.lazy(() => import('./views/pages/productList/ProductList'))
 const NewProduct = React.lazy(() => import('./views/pages/newProduct/NewProduct'))
-const Objective = React.lazy(() => import('./views/pages/objective/Objective'))
+// const Objective = React.lazy(() => import('./views/pages/objective/Objective'))
 const Objective2 = React.lazy(() => import('./views/pages/objective2/Objective2'))
 const CreateNewRef = React.lazy(() => import('./views/pages/objective/createNewRef'))
 const Details = React.lazy(() => import('./views/pages/ViewsDetails/Views'))
-const EditRef = React.lazy(() => import('./views/pages/EditRef/EditRef'))
-
+// const EditRef = React.lazy(() => import('./views/pages/EditRef/EditRef'))
+const EditRefNew = React.lazy(() => import('./views/pages/EditRef/EditRefnew'))
 //Header
 // const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'))
 
@@ -37,7 +37,7 @@ const routes = [
   { path: '/base/cards', name: 'Cards', component: Cards },
   { path: '/configuration', name: 'Configuration', exact: true, component: Configuration },
   { path: '/newProduct', name: 'NewProduct', exact: true, component: NewProduct },
-  { path: '/objective', name: 'Objective', exact: true, component: Objective },
+  // { path: '/objective', name: 'Objective', exact: true, component: Objective },
   { path: '/objective2', name: 'Objective2', exact: true, component: Objective2 },
   {
     path: '/objective2/createNewRef',
@@ -46,7 +46,8 @@ const routes = [
     component: CreateNewRef,
   },
   { path: '/:id/details', name: 'Details', component: Details },
-  { path: '/details/:id/edit', name: 'Edit Reference Objective', exact: true, component: EditRef },
+  // { path: '/details/:id/edit', name: 'Edit Reference Objective', exact: true, component: EditRef },
+  { path: '/details/:id/edit', name: 'Edit Reference Objective', exact: true, component: EditRefNew },
 ]
 
 export default routes
